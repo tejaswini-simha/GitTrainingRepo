6d776fab8388769dc8d50b1102846c37ec16aa09
Tejaswini Simha <tejaswini_y@trigent.com>
2024-12-05 08:26:10 +0530

    Another change

--- src/arithoperators.js
+++ src/arithoperators.js
@@ -4,7 +4,7 @@ let product = 10 * 5;
 let quotient = 10 / 2;
 let diff = 10 - 5;
 console.log(sum, product, diff, quotient);
-console.log("Added another comment");
+console.log("Added another comment1");
 
 // Example 2: Increment operator
 let count = 0;
