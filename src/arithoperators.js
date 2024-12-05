79d33fb8e8921f80358453764bae19699b530642
Tejaswini Simha <tejaswini_y@trigent.com>
2024-12-05 07:49:24 +0530

    Added Diff

--- src/arithoperators.js
+++ src/arithoperators.js
@@ -2,7 +2,8 @@
 let sum = 10 + 5;
 let product = 10 * 5;
 let quotient = 10 / 2;
-console.log(sum, product, quotient);
+let diff = 10 - 5;
+console.log(sum, product, diff, quotient);
 
 // Example 2: Increment operator
 let count = 0;
