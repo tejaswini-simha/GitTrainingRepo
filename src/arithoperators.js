c06b4f8ba4611d4b14315aa3bd56c5136d0b23b0
Tejaswini Simha <tejaswini_y@trigent.com>
2024-12-05 07:46:40 +0530

    First Commit

---
+++ src/arithoperators.js
@@ -0,0 +1,14 @@
+// Example 1: Arithmetic operators
+let sum = 10 + 5;
+let product = 10 * 5;
+console.log(sum, product);
+
+// Example 2: Increment operator
+let count = 0;
+count++;
+console.log(count); // Output: 1
+
+// Example 3: Decrement operator
+let value = 5;
+value--;
+console.log(value); // Output: 4
